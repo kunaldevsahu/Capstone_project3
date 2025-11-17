6f1778989f7640b6ad4d193cca3a1f0c82caf490
kunaldevsahu <kunaldevsahu5@gmail.com>
2025-11-17 17:25:33 +0530

    auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import AuthPage from './pages/AuthPage.jsx';
 import { AuthProvider } from './context/AuthContext.jsx';
-import { BrowserRouter as Router,Route } from 'react-router-dom';
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 
 function App() {
 
@@ -9,8 +9,10 @@ function App() {
     <AuthProvider>
     <>
       <Router>
-        <Route path="/auth" element={<AuthPage />} />
-        <Route path="/dashboard" element={<div>Dashboard Page</div>} />
+        <Routes>
+          <Route path="/auth" element={<AuthPage />} />
+          <Route path="/" element={<div>Dashboard Page</div>} />
+        </Routes>
       </Router>
     </>
     </AuthProvider>
